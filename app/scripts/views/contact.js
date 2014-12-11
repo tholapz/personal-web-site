786c6c77ec48f86f9ef28978ce902342b28d30b0
tholapz <tholapz@gmail.com>
2014-12-11 00:58:25 -0800

    contact form with php endpoint

--- app/scripts/views/contact.js
+++ app/scripts/views/contact.js
@@ -15,14 +15,34 @@ TholapzCom.Views = TholapzCom.Views || {};
 
         className: '',
 
-        events: {},
+        events: {
+            'click button': 'sendmail'
+        },
 
         initialize: function () {
-            this.listenTo(this.model, 'change', this.render);
+            this.render();
+            this.$(".alert-success").hide();
         },
 
         render: function () {
-            this.$el.html(this.template(this.model.toJSON()));
+            this.$el.html(this.template());
+                return this;
+        },
+
+        sendmail: function(e) {
+            e.preventDefault();
+            var formData = this.$("#ajax-contact").serialize();
+            $.ajax({
+                type: 'POST',
+                url: this.$("#ajax-contact").attr('action'),
+                data: formData
+            }).done(_.bind(function(){
+                this.$('#name').val('');
+                this.$('#email').val('');
+                this.$('#message').val('');
+                this.$(".alert-success").show();
+            }, this));
+            
         }
 
     });
